64076affa8e4d20c08abbf7be3f260c41eb59e9b
Dre <bryodiiidah@gmail.com>
2022-12-16 09:01:47 +0300

    upd

--- src/components/Avatar.tsx
+++
@@ -1,9 +0,0 @@
-import MuiAvatar from "@mui/material/Avatar";
-import useIntent from "../hooks/useIntent";
-
-export default function Avatar({ clickAction = "", src }: any) {
-  const handleClick = useIntent({ clickAction });
-  return (
-    <MuiAvatar src={src} sx={{ cursor: "pointer" }} onClick={handleClick}></MuiAvatar>
-  );
-}
--- src/components/TextField.tsx
+++
@@ -1,6 +0,0 @@
-import MuiTextField from "@mui/material/TextField";
-
-export default function TextField(props: any) {
-  return <MuiTextField { ...props } />;
-}
-
